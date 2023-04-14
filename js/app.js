77ea5ccdc390c2b624fb0ef223ca807babaa6437
carlos <carlosleonfranco13@gmail.com>
2023-04-13 19:26:28 -0500

    Validación de form

---
+++ js/app.js
@@ -0,0 +1,87 @@
+const criptomonedasSelect = document.querySelector("#criptomonedas");
+const monedasSelect = document.querySelector("#moneda");
+const formulario = document.querySelector("#formulario");
+
+const objBusqueda = {
+  moneda: "",
+  criptomoneda: "",
+};
+
+// crear un Promise
+const obtenerCriptomonedas = (criptomonedas) =>
+  new Promise((resolve) => {
+    resolve(criptomonedas);
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  consultarCriptomonedas();
+
+  formulario.addEventListener("submit", submitFormulario);
+
+  criptomonedasSelect.addEventListener("change", leerValor);
+  monedasSelect.addEventListener("change", leerValor);
+});
+
+function consultarCriptomonedas() {
+  const url =
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=15&tsym=USD";
+
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => obtenerCriptomonedas(resultado.Data))
+    .then((criptomonedas) => selectCriptomonedas(criptomonedas));
+}
+
+function selectCriptomonedas(criptomonedas) {
+  criptomonedas.forEach((cripto) => {
+    const { FullName, Name } = cripto.CoinInfo;
+
+    const option = document.createElement("OPTION");
+    option.value = Name;
+    option.textContent = FullName;
+    criptomonedasSelect.appendChild(option);
+  });
+}
+
+function leerValor(e) {
+  objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+  e.preventDefault();
+
+  // Validar
+  const { moneda, criptomoneda } = objBusqueda;
+
+  if (moneda === "" || criptomoneda === "") {
+    mostrarAlerta("Ambos campos son obligatorios");
+    return;
+  }
+
+  // Consultar la API con los resultados
+  consultarAPI();
+}
+
+function mostrarAlerta(msg) {
+  const existeError = document.querySelector(".error");
+
+  if (!existeError) {
+    const divMensaje = document.createElement("DIV");
+    divMensaje.classList.add("error");
+
+    // Mensaje de error
+    divMensaje.textContent = msg;
+
+    formulario.appendChild(divMensaje);
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
+
+function consultarAPI() {
+    const { moneda, criptomoneda } = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=COP`;
+}
